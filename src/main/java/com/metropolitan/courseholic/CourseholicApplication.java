8deec77054818100b60636b2d7eae5f6d2376edc
Sergej <sergej.kubat18@gmail.com>
2021-05-21 02:57:02 +0200

    First commit

---
+++ src/main/java/com/metropolitan/courseholic/CourseholicApplication.java
@@ -0,0 +1,13 @@
+package com.metropolitan.courseholic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseholicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseholicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/metropolitan/courseholic/CourseholicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metropolitan.courseholic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseholicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
